a5515ae7fd381a6f556965c76c3ebaece9a7fa98
Xander Jones <xander.jones@tails.com>
2023-11-24 09:51:33 +0000

    chore: simplify import

--- tuya/__main__.py
+++ tuya/__main__.py
@@ -2,16 +2,17 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
-from tuya import config, tray
+from tuya.config import Config
+from tuya.tray import TuyaTray
 
 if __name__ == "__main__":
-    c = config.Config()
+    c = Config()
     c.load_from_file()
 
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
 
-    t = tray.TuyaTray()
+    t = TuyaTray()
     t.show()
 
     app.exec()
